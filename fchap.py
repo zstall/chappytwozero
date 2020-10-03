57a8bd7ff613284a2d0e20df0efe98d4af8b10ac
zach_mac <zstall4@gmail.com>
2020-10-03 07:56:33 -0600

    Added a simple front end

--- fchap.py
+++ fchap.py
@@ -1,9 +1,24 @@
 import chap
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route("/")
+def hello():
+
+    chores = chap.query_db("select name, chore from chores")
+    names = chap.query_db("select fname from users")
+    dic = {}
+    for nm in names:
+        dic[nm[0]]=[]
+
+    for nm in chores:
+        dic[nm[0]]+=[nm[1]]
+
+    return render_template('index.html', chrs=dic)
 
 class choreDone(Resource):
     def get(self, chr_name):
